5ee68661175c7864f19c5a46a98c6a44ee870d77
lokeshboggarapu <lokeshboggarapu@gmail.com>
2017-03-22 21:07:20 -0400

    lok

---
+++ ConsoleApplication14/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class Class1
+    {
+        public bool multiple(int a, int b)
+        
+        {
+            return (b%a)==0;
+           
+        }
+    }
+}
---
+++ ConsoleApplication14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.WriteLine("Enter First Number ");
+            a= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second Number");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Class1 multip = new Class1();
+            bool x = multip.multiple(a, b);
+
+            Console.WriteLine("Result is {0}", x);
+            Console.ReadKey();
+            
+
+        }
+
+
+
+
+    }
+}
